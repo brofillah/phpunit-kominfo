e2ea1dacc2211a68d159a37712eae12c47165b33
Your Name <farhanscience@hotmail.com>
2019-10-11 15:01:05 +0700

    phpunit passed

---
+++ function/aritmatika.php
@@ -0,0 +1,27 @@
+<?php
+
+function tambah($a, $b)
+{
+   return   $a + $b;
+}
+function kurang($a, $b)
+{
+   return $a - $b;
+}
+function kali($a, $b)
+{
+   return $a * $b;
+}
+function bagi($a, $b)
+{
+   return $a / $b;
+}
+function mod($a, $b)
+{
+   return $a % $b;
+}
+function kaliKali($a, $b)
+{
+   return $a ** $b;
+}
+   
---
+++ tests/AritmatikaTest.php
@@ -0,0 +1,15 @@
+<?php
+require("function/aritmatika.php");
+
+class AritmatikaTest extends \PHPUnit\Framework\TestCase
+{
+   public function testTambah()
+   {
+      $this->assertEquals(7, tambah("3", "4"));
+   }
+
+   public function testTypeDataTambah()
+   {
+      $this->assertIsNumeric(tambah("3", "4"));
+   }
+}
